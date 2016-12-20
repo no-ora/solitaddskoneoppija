6f06ef5a056a58dce21064a2a4bae6614960c4a8
no-ora <nooraevainio@gmail.com>
2016-12-20 08:30:04 +0200

    mse

--- first.py
+++ first.py
@@ -20,61 +20,70 @@ def mask(df, f):
   return df[f(df)]
 
 pd.DataFrame.mask = mask
-df = pd.read_csv('data/all-applications-operative-pub-20161031.csv', sep=';',parse_dates=[8,9,10,11,12,13])
+df = pd.read_csv('data/applications-prediction-pub-20161206.csv',parse_dates=['createdDate', 'submittedDate', 'sentDate', 'verdictGivenDate', 'canceledDate'], sep = ';')
 
 #df.head()[df.verdictDays.notnull()]
-df['verdictDays'] = (df.verdictGivenDate - df.submittedDate) / np.timedelta64(1, 'D')
+#df['verdictDays'] = (df.verdictGivenDate - df.submittedDate) / np.timedelta64(1, 'D')
 
 # correct negative verdictDays
-vdMedian = df.loc[(df.verdictDays.notnull()) & (df.verdictDays > 0), 'verdictDays'].quantile(q=0.5)
-df.ix[(df.verdictDays.notnull()) & (df.verdictDays < 0), 'verdictDays'] = vdMedian
+vdMedian = df.loc[(df.leadTimeSubmitted2VerdictGiven.notnull()) & (df.leadTimeSubmitted2VerdictGiven > 0), 'leadTimeSubmitted2VerdictGiven'].quantile(q=0.5)
+df.ix[(df.leadTimeSubmitted2VerdictGiven.notnull()) & (df.leadTimeSubmitted2VerdictGiven < 0), 'verdictDays'] = vdMedian
 # create class variable for classification
-df['verdictClass'] = np.where(df.verdictDays.notnull(), np.where(df.verdictDays <= 7, 1, np.where(df.verdictDays <= 14, 2, 3)), np.nan)
+df['verdictClass'] = np.where(df.leadTimeSubmitted2VerdictGiven.notnull(), np.where(df.leadTimeSubmitted2VerdictGiven <= 7, 1, np.where(df.leadTimeSubmitted2VerdictGiven <= 14, 2, 3)), np.nan)
 
 #df.ix[df.verdictDays <= 7,'verdictClass'] = 1
 #df.ix[(df.verdictDays) <= 14 & (df.verdictDays > 7),'verdictClass'] = 2
 #df.ix[df.verdictDays > 14,'verdictClass'] = 3
 
 
-verdicts = df[df.verdictDays.notnull()]
-v = verdicts.loc[:, ['municipalityId','permitType','operations','verdictDays','verdictClass']]
-v.groupby('municipalityId').count()
+verdicts = df[df.leadTimeSubmitted2VerdictGiven.notnull()]
+# Otetaan vain pientalohakemukset
+#verdicts = verdicts[verdicts['operationId'] == 'pientalo']
+v = verdicts.loc[:, ['municipalityId','permitType','operations','leadTimeSubmitted2VerdictGiven','verdictClass', 'isApplicantLastNameFilled', 'isArchitectCompetenceFilled']]
+v['julkisivupiirros'] = verdicts['n-upload-attachment-julkisivupiirros'] + verdicts['n-upload-attachment-julkisivupiirustus']
+#v.groupby('municipalityId').count()
 
 # build index of municipalities, with count of applications per mun.
-vmun = v.groupby('municipalityId').count().loc[:,['permitType']]
-vmun['applicCount'] = vmun.permitType
-vmun['municipalityId'] = vmun.index
-vmun = vmun.loc[:,['municipalityId','applicCount']]
+# vmun = v.groupby('municipalityId').count().loc[:,['permitType']]
+# vmun['applicCount'] = vmun.permitType
+# vmun['municipalityId'] = vmun.index
+# vmun = vmun.loc[:,['municipalityId','applicCount']]
 # build index of operations, with count of operation in data
-vopers = v.groupby('operations').count().loc[:,['permitType']]
-vopers['operations'] = vopers.index
-vopers['operCount'] = vopers.permitType
-vopers = vopers.loc[:,['operations','operCount']]
+# vopers = v.groupby('operations').count().loc[:,['permitType']]
+# vopers['operations'] = vopers.index
+# vopers['operCount'] = vopers.permitType
+# vopers = vopers.loc[:,['operations','operCount']]
 
 
 # merge secondary info tables with data
-v_ac = pd.merge(v, vmun, on='municipalityId')
-v_ac = pd.merge(v_ac, vopers, on='operations')
+#v_ac = pd.merge(v, vmun, on='municipalityId')
+#v_ac = pd.merge(v_ac, vopers, on='operations')
 # create operations-in-application column
-v_ac['opersinappl'] = v_ac['operations'].apply(lambda x: x.count(",") + 1)
+#v_ac['opersinappl'] = v_ac['operations'].apply(lambda x: x.count(",") + 1)
 
 # with low-frequency municipalities, group together...
 # ie. set 'munip' var, with value =999 (for <100 appls for munip), or =munipId
 
-v_ac['munip'] = np.where(v_ac.applicCount < 100, 999, v_ac.municipalityId)
+#v_ac['munip'] = np.where(v_ac.applicCount < 100, 999, v_ac.municipalityId)
 
 # cleanup extra columns
-v_ac = v_ac.loc[:, ['munip','permitType','operations', 'opersinappl', 'verdictDays','applicCount', 'operCount', 'verdictClass']]
+#v_ac = v_ac.loc[:, ['munip','permitType','operations', 'opersinappl', 'verdictDays','applicCount', 'operCount', 'verdictClass']]
 
 
 # 2) Create dataset of floats with three binary class columns extracted from 'verdictClass'
 # VerdictClass has values 1) <7d verdict time, 2) < 14d, 3) for others
 
-data = v_ac.loc[:, ['opersinappl', 'applicCount', 'operCount']]
-data['opersinappl'] = data['opersinappl'].apply(float)
-data['applicCount'] = data['applicCount'].apply(float)
-data['operCount'] = data['operCount'].apply(float)
-superv = v_ac.loc[:,'verdictClass']
+#data = v.loc[:, ['opersinappl', 'applicCount', 'operCount']]
+#data['opersinappl'] = data['opersinappl'].apply(float)
+#data['applicCount'] = data['applicCount'].apply(float)
+#data['operCount'] = data['operCount'].apply(float)
+# OTA: createdMonth, operationId, architectFilled, upload*, kunta
+# Regressiot leadTimeSubmitted2VerdictGiven
+data = v.loc[:, ['isApplicantLastNameFilled', 'isArchitectCompetenceFilled', 'julkisivupiirros']]
+data['isApplicantLastNameFilled'] = data['isApplicantLastNameFilled'].apply(float)
+data['isArchitectCompetenceFilled'] = data['isArchitectCompetenceFilled'].apply(float)
+data['julkisivupiirros'] = data['julkisivupiirros'].apply(float)
+superv = v.loc[:,'verdictClass']
 enc = preprocessing.OneHotEncoder()
 enc.fit(superv.values.reshape(-1,1))
 vv = enc.transform(superv.values.reshape(-1,1))
@@ -91,7 +100,7 @@ clf.fit(TR, TRs)
 pred = clf.predict(data[train:].get_values())
 
 # 4) Count mean squared error for results
-pred_data = v_ac.loc[train:]
+pred_data = v[train:]
 pred_data['predicted_verdictClass'] = np.where(pred[:, 0] == 1, 1.0, np.where(pred[:, 1] == 1, 2.0, 3.0)) 
 mse = ((pred_data['predicted_verdictClass'] - pred_data['verdictClass']) ** 2).mean(axis=None)
 
